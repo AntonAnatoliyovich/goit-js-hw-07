002c6fd2c9192912132e1b29cecc285966f8fb2c
AntonAnatoliyovich <anton.shadura@gmail.com>
2023-01-19 17:36:09 +0200

    gallery-02.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const itemsMarkup = createGallery(galleryItems);
+
+function createGallery(items) {
+    return items.map(({original, preview, description}) => {
+        return `<div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+            />
+        </a>
+        </div>`
+    }).join('');
+}
+
+galleryContainer.innerHTML = itemsMarkup;
+
+new SimpleLightbox('.gallery .gallery__link', {'captionsData': 'alt', 'captionDelay': 250});
